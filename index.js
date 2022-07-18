d1341e8ebabae4c49594444f80964ae276f97460
soorejvs <102665581+soorejvs@users.noreply.github.com>
2022-07-18 10:15:26 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const BookData = require("./src/model/BookData");
 const cors = require("cors");
-//const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 var app = new express();
 
 
@@ -9,11 +9,25 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
-//username='admin'
-//password='123456'
+username='Admin'
+password='123456'
 //const path=require('path');
 //app.user(express.static(/dist/front-end));
 
+app.post('/login', (req, res) => {
+    let userData = req.body
+        if (!username) {
+          res.status(401).send('Invalid Username')
+        } else 
+        if ( password !== userData.password) {
+          res.status(401).send('Invalid Password')
+        } else {
+          let payload = {subject: username+password}
+          let token = jwt.sign(payload, 'secretKey')
+          res.status(200).send({token})
+        }
+      
+    })
 // API
 app.get('/books', function (req, res) {
     res.header("Access-Control-Allow-Origin", "*");
